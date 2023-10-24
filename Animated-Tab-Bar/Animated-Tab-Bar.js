8c9c37c8a761aa9ca0d5d8c0254323031649411f
fatima rahimi <iamfatimarahimi@gmail.com>
2023-10-24 19:15:38 +0330

    clickItem Function

--- Animated-Tab-Bar/Animated-Tab-Bar.js
+++ Animated-Tab-Bar/Animated-Tab-Bar.js
@@ -6,3 +6,13 @@ const menu = body.querySelector(".menu");
 const menuItems = menu.querySelectorAll(".menu__item");
 const menuBorder = menu.querySelector(".menu__border");
 let activeItem = menu.querySelector(".active");
+
+function clickItem(item,index){
+    menu.computedStyleMap.removeProperty("--timeOut");
+    if(activeItem==item)
+    return;
+
+    if(activeItem){
+        activeItem.classList.remove("active");
+    }
+}
