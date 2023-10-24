f02a68df8a30a18bd9eae0bae25fb8c0c0d08a62
fatima rahimi <iamfatimarahimi@gmail.com>
2023-10-24 19:24:19 +0330

    menuItems.forEach(

--- Animated-Tab-Bar/Animated-Tab-Bar.js
+++ Animated-Tab-Bar/Animated-Tab-Bar.js
@@ -1,25 +1,31 @@
-"use strict";
+"use strict"; 
 
-const body =document.body;
+const body = document.body;
 const bgColorsBody = ["#ffb457", "#ff96bd", "#9999fb", "#ffe797", "#cffff1"];
 const menu = body.querySelector(".menu");
 const menuItems = menu.querySelectorAll(".menu__item");
 const menuBorder = menu.querySelector(".menu__border");
 let activeItem = menu.querySelector(".active");
 
-function clickItem(item,index){
-    menu.computedStyleMap.removeProperty("--timeOut");
-    if(activeItem==item)
-    return;
+function clickItem(item, index) {
 
-    if(activeItem){
+    menu.style.removeProperty("--timeOut");
+    
+    if (activeItem == item) return;
+    
+    if (activeItem) {
         activeItem.classList.remove("active");
     }
+
+    
     item.classList.add("active");
     body.style.backgroundColor = bgColorsBody[index];
     activeItem = item;
-    offsetMenuBorder(activeItem,menuBorder);
+    offsetMenuBorder(activeItem, menuBorder);
+    
+    
 }
+
 function offsetMenuBorder(element, menuBorder) {
 
     const offsetActiveItem = element.getBoundingClientRect();
@@ -27,3 +33,16 @@ function offsetMenuBorder(element, menuBorder) {
     menuBorder.style.transform = `translate3d(${left}, 0 , 0)`;
 
 }
+
+offsetMenuBorder(activeItem, menuBorder);
+
+menuItems.forEach((item, index) => {
+
+    item.addEventListener("click", () => clickItem(item, index));
+    
+})
+
+window.addEventListener("resize", () => {
+    offsetMenuBorder(activeItem, menuBorder);
+    menu.style.setProperty("--timeOut", "none");
+});
