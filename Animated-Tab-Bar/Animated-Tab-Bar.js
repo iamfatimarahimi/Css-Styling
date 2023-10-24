100b6dc769342975a11e7075ec9296d5c293e3f2
fatima rahimi <iamfatimarahimi@gmail.com>
2023-10-24 19:13:21 +0330

    Adding the elements to js file

---
+++ Animated-Tab-Bar/Animated-Tab-Bar.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const body =document.body;
+const bgColorsBody = ["#ffb457", "#ff96bd", "#9999fb", "#ffe797", "#cffff1"];
+const menu = body.querySelector(".menu");
+const menuItems = menu.querySelectorAll(".menu__item");
+const menuBorder = menu.querySelector(".menu__border");
+let activeItem = menu.querySelector(".active");
